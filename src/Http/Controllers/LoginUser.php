9288da87267dc1d03fb6fc40a0502e2e72b06869
leepownall <leepownall@users.noreply.github.com>
2020-09-06 19:53:50 +0000

    Fix styling

--- src/Http/Controllers/LoginUser.php
+++ src/Http/Controllers/LoginUser.php
@@ -8,7 +8,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\URL;
 use Pownall\MagicLogin\Exceptions\CouldNotLogin;
 use Pownall\MagicLogin\UserClass;
 
